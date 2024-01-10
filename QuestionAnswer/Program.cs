eb9551fc9d61d29ee1c07373763fe1015dae3c24
jane rose <rosejaneforyou@gmail.com>
2024-01-10 11:49:11 +0300

    About Interfaces

--- QuestionAnswer/Program.cs
+++ QuestionAnswer/Program.cs
@@ -1,10 +1,11 @@
 ﻿//1 program to print hello and your name in a separate line
-string firstname = "Alexandra ";
-string lastname = "Abramov";
-string name = firstname + lastname;
 
-Console.WriteLine("Hello: ");
-Console.WriteLine(name);
+//string firstname = "Alexandra ";
+//string lastname = "Abramov";
+//string name = firstname + lastname;
+
+//Console.WriteLine("Hello: ");
+//Console.WriteLine(name);
 
 
 //2 program to print sum of two numbers
@@ -443,42 +444,675 @@ Console.WriteLine(name);
 
 
 //23 converts a given string to lowercase
-Console.WriteLine("Enter Your String");
-string userInput = Console.ReadLine();
+//Console.WriteLine("Enter Your String");
+//string userInput = Console.ReadLine();
 
-Lower lower = new Lower();
-string result = lower.toLower(userInput);
-Console.WriteLine(result);
-    class Lower
-{
-    public string toLower(string input)
-    {
-        string? userInput = "";
-        if (string.IsNullOrEmpty(input))// or (input == null)
-        {
-            Console.WriteLine("Must Provide a Valid String");
-        }
-        return input.ToLower();
-    }
-}
+//Lower lower = new Lower();
+//string result = lower.toLower(userInput);
+//Console.WriteLine(result);
+//class Lower
+//{
+//    public string toLower(string input)
+//    {
+//        string? userInput = "";
+//        if (string.IsNullOrEmpty(input))// or (input == null)
+//        {
+//            Console.WriteLine("Must Provide a Valid String");
+//        }
+//        return input.ToLower();
+//    }
+//}
 
 
 //33 program to check if a given positive number is a multiple of 3 or 7
-Console.WriteLine("Enter a Digit");
-int num = Console.ReadLine();
 
-Check check = new Check();
-int results = check.checkMultiple(num);
-Console.WriteLine(results);
-class Check
+//Console.WriteLine("Enter a Digit");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//Check check = new Check();
+//int results = Convert.ToInt32(check.checkMultiple(num));
+//Console.WriteLine(results);
+//class Check
+//{
+//    public bool checkMultiple(int num)
+//    {
+//        while (num > 0)
+//        {
+//            if (num % 3 == 0)
+//            {
+//                Console.WriteLine("Number Is Divisible By 3");
+//                break;
+//            }
+//            else if (num % 7 == 0)
+//            {
+//                Console.WriteLine("Number Is Divisible By 7");
+//                break;
+//            }
+//            else
+//            {
+//                Console.WriteLine("Number Not Divisible By Either 3 or 7");
+//                break;
+//            }
+
+//        }
+//        if (num <= 0)
+//        {
+//            Console.WriteLine("Input Must Be a Positive Integer");
+//        }
+//        return (num % 3 == 0 || num % 7 == 0);
+//    }
+//}
+
+
+//54 program to find the century of a year
+//Console.WriteLine("Enter A Year");
+//int year = Convert.ToInt32(Console.ReadLine());
+
+//Century century = new Century();
+//double results = Convert.ToInt32(century.checkForCentury(century));
+//Console.WriteLine(results);
+
+//class Century
+//{
+//    public double checkForCentury(int year)
+//    {
+//        return (year / 100) + ((year % 100 == 0) ? 0 : 1);
+//    }
+//}
+
+
+//using get set properties
+
+//call the method person
+//Person person = new Person();
+//person.Name = "Jane Rose";
+
+//Console.WriteLine(person.Name);
+
+//create class person and method name
+//class Person
+//{
+//    private string name;
+
+//    public string Name
+//    {
+//        get {return name;}
+//        set { name = value;}
+//    }
+//}
+
+
+//program that sorts items in a list by ascending and descending order
+
+//create a list of integers
+
+//List<int> numbers = new List<int>();
+
+//Console.WriteLine("Enter Integers One By One (or enter 'Q' to Quit) : ");
+
+
+//while (true)
+//{
+//    string input = Console.ReadLine();
+
+//    if (input == "Q")
+//    { break; }
+
+//attempts to convert the user input
+//bool conversionSuccessful = int.TryParse(input, out int number);
+
+//if (conversionSuccessful)
+//{
+//    numbers.Add(number);
+//}
+//else //conversion failed
+//    {
+//        Console.WriteLine("Invalid Input Please Enter a Valid Integer or Press Q to quit");
+//    }
+//}
+
+//if (numbers.Count > 0)
+//{
+//    Console.WriteLine("\nChoose Sorting Order");
+//    Console.WriteLine("1. Ascending Order");
+//    Console.WriteLine("2. Descending Order");
+
+//    string orderChoice = Console.ReadLine();
+
+//    switch (orderChoice)
+//    {
+//        case "1":
+//            numbers.Sort();
+//            Console.WriteLine("\nSorted in Ascending Order");
+//            PrintList(numbers);
+//            break;
+
+//            case "2":
+//            numbers.OrderBy(n => n);
+//            Console.WriteLine("\nSorted in Descending Order");
+//            PrintList(numbers);
+//            break;
+
+//        default:
+//            Console.WriteLine("Invalid Choice. Exit");
+//            break;
+//    }
+//}
+// a method that prints the list (PrintList)
+//static void PrintList(List<int> list)
+//{
+//    foreach (var item in list)
+//    {
+//        Console.Write($"{item} ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+//program that checks either a number is a multiple of 3, 5 or neither (fizz for 3, buzz for 5, fizzbuzz for both)
+//using System;
+
+//Console.WriteLine("Enter a digit");
+//int numb = Convert.ToInt32(Console.ReadLine());
+
+//Check check = new Check();
+//int results = Convert.ToInt32(check.CheckMultiple(numb));
+
+//Console.WriteLine(results);
+//class Check
+//{
+//    public string CheckMultiple(int numb)
+//    {
+//        if (numb % 3 == 0 && numb % 5 == 0)
+//        {
+//            return "FizzBuzz";
+//        }
+//        else if (numb % 3 == 0)
+//        {
+//            return "Fizz";
+//        }
+//        else if (numb % 5 == 0)
+//        {
+//            return "Buzz";
+//        }
+//        else
+//        {
+//            return "Neither Fizz Nor Buzz";
+//        }
+//    }
+//}
+
+//Write a C# Sharp program to get the number of letters and digits in a given string.
+//create two methods, count letters and count digits
+
+//class StringAnalyzer
+//{
+//    public int CountLetters()
+//    {
+//        int letterCount = 0;
+//        foreach (char c in input)
+//        {
+//            if (char.IsLetter(c))
+//            {
+//                letterCount++;
+//            }
+//        }
+//        return letterCount;
+//    }
+
+//    public int CountDigits()
+//    {
+//        int digitCount = 0;
+//        foreach (char c in input)
+//        {
+//            if (char.IsDigit(c))
+//            {
+//                digitCount++;
+//            }
+//        }
+//        return digitCount;
+//    }
+//}
+//class CallMethods
+//{
+//    public void calling()
+//    {
+//        Console.Write("Enter a String");
+//        string input = Console.ReadLine();
+
+//        StringAnalyzer stringAnalyzer = new StringAnalyzer();
+
+//        stringAnalyzer.CountLetters();
+//        stringAnalyzer.CountDigits();
+//    }
+//}
+
+
+//program that converts minutes into seconds (using constructors)
+//class MinutesConverter
+//{
+//    private int minutes;
+
+//    public MinutesConverter(int minutes)
+////    {
+////        this.minutes = minutes;
+////    }
+////    public int ConvertToSeconds()
+////    {
+////        //1 min = 60 sec
+////        return minutes * 60;
+////    }
+////}
+
+////class Program
+////{
+////    static void Main()
+////    {
+////        Console.WriteLine("Enter Number of Minutes");
+////        int minutes = Convert.ToInt32(Console.ReadLine());
+
+////        MinutesConverter converter = new MinutesConverter(minutes);
+////        int seconds = converter.ConvertToSeconds();
+
+////        Console.WriteLine($"{minutes} minutes is equal to {seconds} seconds");
+////    }
+
+////}
+
+
+//program that convert age to days (using constructors)
+//class AgeConverter
+//{
+//    private int years;
+
+//    public AgeConverter(int years)
+//    {
+//        this.years = years;
+//    }
+//    public int ConvertToSeconds()
+//    {
+//        //1 years = 365 days
+//        return years * 365;
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Enter Number of Years");
+//        int years = Convert.ToInt32(Console.ReadLine());
+
+//        AgeConverter converter = new AgeConverter(years);
+//        int days = converter.ConvertToSeconds();
+
+//        Console.WriteLine($"{years} years is equal to {days} days");
+//    }
+
+//}
+
+
+//program to check whether a word is plural of not plural
+//class CheckPlural
+//{
+//    public bool IsPlural(string word)
+//    {
+//        //conditions; if the word is plural return true else false
+//        if (word.EndsWith("s") || word.EndsWith("es"))
+//        {
+//            return true;
+//        }
+//        else
+//        {
+//            return false;
+//        }
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Enter a Word");
+//        string inputWord = Console.ReadLine();
+
+//        CheckPlural checkPlural = new CheckPlural();
+
+//        if (checkPlural.IsPlural(inputWord))
+//        {
+//            Console.WriteLine($"{inputWord} Is Plural");
+//        }
+//        else 
+//        { 
+//            Console.WriteLine($"{inputWord} Is Not Plural");
+//        }
+//    }
+//}
+
+//using System;
+
+//public class UserProfile
+//{
+//    private string email;
+//    private string password;
+//    private string languagePreference;
+
+//    public void InputCredentials()
+//    {
+//        Console.Write("Enter your email: ");
+//        email = Console.ReadLine();
+
+//        Console.Write("Enter your password: ");
+//        password = Console.ReadLine();
+
+//        Console.Write("Enter your language preference: ");
+//        languagePreference = Console.ReadLine();
+
+//        Console.WriteLine("Credentials set successfully");
+//    }
+
+//    public void DisplayCredentials()
+//    {
+//        Console.WriteLine($"Your current email is: {email}");
+//        Console.WriteLine($"Your current password is: {password}");
+//        Console.WriteLine($"Your current language preference is: {languagePreference}");
+//    }
+
+//    public void ResetCredentials()
+//    {
+//        Console.Write("Enter a new email: ");
+//        email = Console.ReadLine();
+
+//        Console.Write("Enter a new password: ");
+//        password = Console.ReadLine();
+
+//        Console.Write("Enter a new language preference: ");
+//        languagePreference = Console.ReadLine();
+
+//        Console.WriteLine("Credentials reset successfully");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        UserProfile user = new UserProfile();
+
+//        Console.WriteLine("Options:");
+//        Console.WriteLine("1. Input Initial Credentials");
+//        Console.WriteLine("2. View Credentials");
+//        Console.WriteLine("3. Reset Credentials");
+
+//        Console.Write("Select an option (1, 2, or 3): ");
+//        string option = Console.ReadLine();
+
+//        switch (option)
+//        {
+//            case "1":
+//                user.InputCredentials();
+//                break;
+//            case "2":
+//                user.DisplayCredentials();
+//                break;
+//            case "3":
+//                user.ResetCredentials();
+//                user.DisplayCredentials();
+//                break;
+//            default:
+//                Console.WriteLine("Invalid option. Please select 1, 2, or 3.");
+//                break;
+//        }
+//    }
+//}
+
+//class Cart
+//{
+//    public void DisplayShopItems() //method that displays the items in the shop
+//    {
+//        Console.WriteLine("Shop Items:");
+//        Console.WriteLine("1. Burger - 550");
+//        Console.WriteLine("2. Fries - 300");
+//        Console.WriteLine("3. Smoothie - 350");
+//    }
+//    public void AddToCart(List<string> shoppingCart, int itemNumber) //this adds the items chosen in the cart
+//    {
+
+//        Console.Write("Enter the item number to add to the cart (or 0 to go back): ");
+//        string? itemChoice = Console.ReadLine();
+
+//        if (int.TryParse(itemChoice, out itemNumber) && itemNumber >= 1 && itemNumber <= 3) //*to check
+//        {
+//            AddToCart(shoppingCart, itemNumber);
+//            Console.WriteLine("Item added to the cart!");
+//        }
+//        else if (itemNumber != 0)
+//        {
+//            Console.WriteLine("Invalid item number. Please try again.");
+//        }
+
+//        switch (itemNumber)
+//        {
+//            case 1:
+//                shoppingCart.Add("Burger - 550");
+//                break;
+//            case 2:
+//                shoppingCart.Add("Fries - 300");
+//                break;
+//            case 3:
+//                shoppingCart.Add("Smoothie - 350");
+//                break;
+//            case 4:
+//                Console.WriteLine("Shop Page");
+//                DisplayShopItems();
+//                break;
+//        }
+//    }
+//    public void DisplayShoppingCart(List<string> shoppingCart)
+//    {
+//        Console.WriteLine("Shopping Cart:");
+//        if (shoppingCart.Count == 0)
+//        {
+//            Console.WriteLine("Your cart is empty.");
+//        }
+//        else
+//        {
+//            foreach (var item in shoppingCart)
+//            {
+//                Console.WriteLine($"- {item}");
+//            }
+//        }
+//    }
+/*    public void DisplayPaymentMethods()*/ //method to show user the payment otions
+//    {
+//        Console.WriteLine("Payment Methods:");
+//        Console.WriteLine("1. Credit Card");
+//        Console.WriteLine("2. PayPal");
+//        Console.WriteLine("3. Mpesa");
+//    }
+//}
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace AnOrderApp
+//{
+//    public class Shop
+//    {
+//        private List<string> shoppingItems;
+//        public void shopItems()
+//        {
+//            List<string> shoppingItems = new List<string> { "Pizza", "Burger", "Fruit Smoothie" };
+//        }
+
+//        public void displayShopItems()
+//        {
+//            if (shoppingItems != null)
+//            {
+//                Console.WriteLine("Available Shop Items");
+
+//                foreach (string item in shoppingItems)
+//                {
+//                    Console.WriteLine(item);
+//                }
+//            }
+//            else { Console.WriteLine("Available Items is Empty"); }
+
+//        }
+//    }
+//    class MethodCall
+//    {
+//        public MethodCall()
+//        {
+//            Shop shop = new Shop();
+
+//            shop.displayShopItems();
+
+//            shop.shopItems();
+//        }
+//    }
+//}
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace AnOrderApp
+//{
+//    class Cart
+//    {
+//        private List<string> cartItems;
+//        private List<string> paymentMethods;
+
+//        public void DisplayCartItems()
+//        {
+//            MethodCall methodCall = new MethodCall();
+
+//            List<string> cartItems = new List<string>(); 
+
+//            if (cartItems.Count == 0)
+//            {
+//                Console.WriteLine("Your cart is empty.");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Items in your cart:");
+//                foreach (var item in cartItems)
+//                {
+//                    Console.WriteLine(item);
+//                }
+//            }
+//        }
+//        public void AddToCart(string item)
+//        {
+//            cartItems.Add(item);
+//            Console.WriteLine($"{item} added to cart!");
+//        }
+//        public void DisplayPaymentMethods()
+//        {
+//            cartItems = new List<string>();
+//            paymentMethods = new List<string>() { "visa", "Mpesa", "PayPal" };
+
+//            foreach (var item in paymentMethods)
+//            {
+//                cartItems.Add(item);
+//            }
+//            Console.WriteLine("Choose a Payment Method");
+//        }
+
+//    }
+
+//    class CallMethods
+//    {
+//        public CallMethods()
+//        {
+//            Cart cart = new Cart();
+
+//            cart.DisplayCartItems();
+
+//            cart.DisplayPaymentMethods();
+//        }
+//    }
+
+//}
+
+
+//public void DisplayPaymentMethods() //method to show user the payment otions
+//{
+//    Console.WriteLine("Payment Methods:");
+//    Console.WriteLine("1. Credit Card");
+//    Console.WriteLine("2. PayPal");
+//    Console.WriteLine("3. Mpesa");
+//}
+
+//public class Logger
+//{
+//    public void Log(string message)
+//    {
+//        Console.WriteLine(message);
+//    }
+//}
+//public class Person
+//{
+//    public Person(string name) //constructor class
+//    {
+//        var logger = new Logger();
+//        logger.Log($"{name} was created"); //person is dependent on the logger class
+//    }
+//}
+
+//public class Person
+//{
+//    public Person(string name, Logger logger)
+//    {
+//        //now newing up [var logger = new Logger();]
+//        logger.Log($"{name} was created");
+//    }
+//}
+
+
+interface IAnimal //interface named IAnimal and declare abstract methods
 {
-    public bool checkMultiple()
+    void MakeSound();
+    void Eat();
+}
+class Dog : IAnimal
+{
+    public void MakeSound() //declare an implementation of the MakeSound method
     {
-        int num = 0;
-        if (num <= 0)
-        {
-            Console.WriteLine("Input Must Be a Positive Integer");
-        }
-        return (num % 3 == 0 || num % 7 == 0);
+        Console.WriteLine("Woof");
+    }
+    public void Eat() //declare an implementation of the Eat method
+    {
+        Console.WriteLine("Pellets");
+    }
+}
+class Cow : IAnimal // same thing
+{
+    public void MakeSound() 
+    {
+        Console.WriteLine("Moo");
+    }
+    public void Eat() 
+    {
+        Console.WriteLine("Grass");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog(); //instance of the dog class
+        dog.MakeSound(); //call the methods form the Interface IAnimal
+        dog.Eat();
+
+        Cow cow = new Cow();
+        cow.MakeSound();
+        cow.Eat();
     }
 }
